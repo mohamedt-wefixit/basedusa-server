d1aaea6df965f36d583f194e364ca80012a21015
mohamedt-wefixit <157041505+mohamedt-wefixit@users.noreply.github.com>
2024-06-21 16:26:04 +0100

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { ConnectButton } from "thirdweb/react";
+import { client } from "./client";
+import { createWallet } from "thirdweb/wallets";
+import { defineChain } from "thirdweb";
+import { baseSepolia } from "thirdweb/chains";
+
+export default function Home() {
+  const wallets =[
+    createWallet('com.coinbase.wallet')
+  ]
+  return (
+    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
+      <div className="py-20">
+
+          <ConnectButton
+            client={client}
+            wallets={wallets}
+            chain={defineChain(baseSepolia)}
+
+          />
+
+      </div>
+    </main>
+  );
+}
