fd73f7bb668258010df2e2f120497ad2401e488c
mohamedt-wefixit <157041505+mohamedt-wefixit@users.noreply.github.com>
2024-06-26 04:00:52 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     createWallet('com.coinbase.wallet')
   ]
   return (
-    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
+    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto bg-[#E93734]">
       <div className="py-20">
 
           <ConnectButton
